3636bad50df9d20d43d4e23639855a565cf685cf
Paladin <worldtier1@gmail.com>
2022-07-20 15:05:38 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
             //route giong trong cai weatherforecast (controller)
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
